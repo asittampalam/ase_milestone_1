73a6fe3a800b35558f07380c02ac5dfec794ede1
Arun Sittampalam <arun@sittampalam.ch>
2014-09-19 15:48:43 +0200

    5.10 done.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,12 +1,16 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def show
